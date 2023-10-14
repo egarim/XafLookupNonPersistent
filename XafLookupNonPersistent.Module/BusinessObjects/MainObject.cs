ee8bb019a50717259d6de48d965928ac6ff9f2fb
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-10-14 12:20:31 +0300

    Add project files.

---
+++ XafLookupNonPersistent.Module/BusinessObjects/MainObject.cs
@@ -0,0 +1,108 @@
+﻿using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafLookupNonPersistent.Module.BusinessObjects
+{
+    using DevExpress.ExpressApp;
+    using DevExpress.ExpressApp.DC;
+    using DevExpress.Persistent.Base;
+    using System.ComponentModel;
+
+    // ...
+    [DomainComponent]
+    public class MainObject : NonPersistentBaseObject
+    {
+        ITypeInfo typeInfo;
+        PropertyObject propertyObject;
+        private String name;
+        private String description;
+        public override void OnSaving()
+        {
+            base.OnSaving();
+            // ...
+        }
+        public String Name
+        {
+            get { return name; }
+            set { SetPropertyValue(ref name, value); }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { SetPropertyValue(ref description, value); }
+        }
+        [DataSourceProperty("RefProperties")]
+        public PropertyObject PropertyObject
+        {
+            get => propertyObject;
+            set => SetPropertyValue<PropertyObject>(ref propertyObject, value, nameof(PropertyObject));
+        }
+        [Browsable(false)]
+        public ITypeInfo TypeInfo
+        {
+            get => typeInfo;
+            set
+            {
+                if (typeInfo == value)
+                    return;
+                typeInfo = value;
+                OnPropertyChanged(nameof(TypeInfo));
+            }
+        }
+        
+        [Browsable(false)]
+        public List<PropertyObject> RefProperties
+        {
+            get
+            {
+
+                //TODO we need to remove service members https://supportcenter.devexpress.com/ticket/details/q237276/what-is-servicefield
+                List<PropertyObject> list = new List<PropertyObject>();
+                TypeInfo.Members.Where(x => x.IsPersistent).ToList().ForEach(x =>
+                {
+                    var ServiceField = x as DevExpress.Xpo.Metadata.Helpers.ServiceField;
+                    if(ServiceField==null)
+                    {
+                        var PropertyObject = ObjectSpace.CreateObject<PropertyObject>();
+                        PropertyObject.Name = x.Name;
+                        PropertyObject.Description = x.DisplayName;
+                        list.Add(PropertyObject);
+                    }
+                  
+                    //list.Add(PropertyObject);
+                });
+                //PropertyObject1.Name = "1";
+                //PropertyObject2.Name = "2";
+                //list.Add(PropertyObject1);
+                //list.Add(PropertyObject2);
+                return list;
+                //return this.ObjectSpace.CreateCollection(typeof(PropertyObject)).Cast<PropertyObject>().ToList();
+            }
+        }
+    }
+    [DomainComponent]
+    public class PropertyObject : NonPersistentBaseObject
+    {
+        private String name;
+        private String description;
+        public override void OnSaving()
+        {
+            base.OnSaving();
+            // ...
+        }
+        public String Name
+        {
+            get { return name; }
+            set { SetPropertyValue(ref name, value); }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { SetPropertyValue(ref description, value); }
+        }
+    }
+}
---
+++ XafLookupNonPersistent.Module/BusinessObjects/MainVc.cs
@@ -0,0 +1,59 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafLookupNonPersistent.Module.BusinessObjects
+{
+    public class MainVc : ViewController
+    {
+        PopupWindowShowAction ShowPopup;
+        public MainVc() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            ShowPopup = new PopupWindowShowAction(this, "ShowPopup", "View");
+            ShowPopup.Execute += ShowPopup_Execute;
+            ShowPopup.CustomizePopupWindowParams += ShowPopup_CustomizePopupWindowParams;
+
+        }
+        private void ShowPopup_Execute(object sender, PopupWindowShowActionExecuteEventArgs e)
+        {
+            var selectedPopupWindowObjects = e.PopupWindowViewSelectedObjects;
+            var selectedSourceViewObjects = e.SelectedObjects;
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112723/).
+        }
+        private void ShowPopup_CustomizePopupWindowParams(object sender, CustomizePopupWindowParamsEventArgs e)
+        {
+            var Os = this.Application.CreateObjectSpace(typeof(MainObject));
+            var MainInstance = Os.CreateObject<MainObject>();
+            MainInstance.TypeInfo = this.View.ObjectTypeInfo;
+            //var PropertyObject1 = Os.CreateObject<PropertyObject>();
+            //var PropertyObject2 = Os.CreateObject<PropertyObject>();
+            //PropertyObject1.Name = "1";
+            //PropertyObject2.Name = "2";
+
+            Os.CommitChanges();
+            var View = Application.CreateDetailView(Os, MainInstance);
+            e.View = View;
+            // Set the e.View parameter to a newly created view (https://docs.devexpress.com/eXpressAppFramework/112723/).
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+}
